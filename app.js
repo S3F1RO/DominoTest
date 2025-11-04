1283fa5211d1ee8cf1e0532603a29eefdb8e3cd0
Splenchy <benjamin.macabou@gmail.com>
2025-11-04 18:51:10 -0300

    addhtmlJs

--- app.js
+++ app.js
@@ -9,20 +9,70 @@ document.getElementById("connectBtn").addEventListener("click", () => {
     socket = new WebSocket("ws://10.122.7.180:8000");
     socket.onopen = () => {
         document.getElementById("status").textContent = "Connecté ";
-        log("Connecté au  serveur WebSocket");
+        document.getElementById("status").style.color="green"
+
+        log("Connecté au serveur WebSocket");
         socket.send(JSON.stringify({type: "hello", payload: "Bonjour serveur"
         }));
 };
 socket.onmessage = (event) => {
-    log("Reçu du serveur : " + event.data);
+    data = JSON.parse(event.data);
+    log("Joueur " + data.player);
+    var hands = data.hands;
+    const formattedHands = hands.map(d => `(${d[0]}) (${d[1]}) (${d[2]}) (${d[3]}) (${d[4]}) (${d[5]}) (${d[6]})`).join(" ");
+    log("Tu as une main de " + formattedHands);
 };
 
 socket.onclose = () => {
     document.getElementById("status").textContent = "Déconnecté ";
+    var status = document.getElementById("status");
+    status.style.color="red";
     log("Connexion fermée");
 };
 
 socket.onerror = (error) => {
     log("Erreur WebSocket : " + error);
 };
+
+document.getElementById("joinBtn").addEventListener("click", () => {
+    socket.send(JSON.stringify({type: "join"}));
+});
+
+
+
+
+
+document.getElementById("domino").addEventListener("keypress", () => {
+    domino = domino.value;
+});
+document.getElementById("sideBtn").addEventListener("click", () => {
+    side = "left";
+
+    var all = document.getElementById("button");
+    all.style.color="black";
+
+    var focus = document.getElementById("sideBtn");
+    focus.style.color="blue";
+
+    console.log(side)
+});
+
+document.getElementById("side2Btn").addEventListener("click", () => {
+    side = "right"
+
+    var all = document.getElementById("button");
+    all.style.color="black";
+
+    var focus = document.getElementById("side2Btn");
+    focus.style.color="blue";
+});
+document.getElementById("playBtn").addEventListener("click", () => {
+    socket.send(JSON.stringify({type : "play", "domino" : domino, "side" : side}));
+    // socket.s
+});
+
+document.getElementById("passBtn").addEventListener("click", () => {
+    socket.send(JSON.stringify({type : "pass"}))
+});
+
 });
