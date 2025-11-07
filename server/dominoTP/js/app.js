ad8026436ac1da2aed466efd8c5ea60ee5275e76
Splenchy <benjamin.macabou@gmail.com>
2025-11-07 13:09:39 -0300

    New Ver+int

--- server/dominoTP/js/app.js
+++ server/dominoTP/js/app.js
@@ -4,48 +4,104 @@ function log(msg) {
     const logEl = document.getElementById("log")
     logEl.textContent += msg + "\n";
 }
+// setTimeout(function() {
+//         document.getElementById("log").textContent="";
+//     }, 15000);
 document.getElementById("disconnectBtn").style.display = "none";
 
 document.getElementById("connectBtn").addEventListener("click", () => {
-    socket = new WebSocket("ws://10.122.7.180:8000");
+    socket = new WebSocket("ws://10.122.7.16:8000");
     socket.onopen = () => {
         document.getElementById("status").textContent = "Connecté ";
         document.getElementById("status").style.color="rgb(12,204,12)";
         document.getElementById("connectBtn").style.display="none";
+        // document.getElementById("dominoInput").style.display="block";
+        // document.getElementById("inputTitle").style.display="block";
         log("Connecté au serveur WebSocket");
         socket.send(JSON.stringify({type: "hello", payload: "Bonjour serveur"
         }));
 };
 
+
+const playerName = document.getElementById("playerName");
+document.getElementById("playerName").addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        log('Nom enregistré ' + playerName.value);
+        socket.send(JSON.stringify({type: "userName", payload: playerName.value}));
+        playerName.style.display = 'none';
+    }
+}); 
+
+const emotes = {
+  ":smile:": "https://twemoji.maxcdn.com/v/latest/72x72/1f604.png",
+  ":heart:": "https://twemoji.maxcdn.com/v/latest/72x72/2764.png",
+  ":thumbsup:": "https://twemoji.maxcdn.com/v/latest/72x72/1f44d.png"
+};
 document.getElementById("chatInput").addEventListener('keydown', function(event) {
     const chatInput = document.getElementById("chatInput")
+    const message = chatInput.value.trim();
     if (event.key == 'Enter') {
-        socket.send(JSON.stringify(chatInput.value));
+        socket.send(JSON.stringify({type: "chat", payload: chatInput.value}));
+        document.getElementById("chatInput").value = "";
+        // const div = document.createElement('div');
+        // const listChats = document.createElement('ul');
+        // listChats.id = 'chatList';
+        // const li = document.createElement('li');
+        // li.innerHTML = '<strong>' + playerName.value + ' : </strong> ' + message;
+        // document.getElementById("chatDisplay").append(li);
+        // document.getElementById("chatDisplay").scrollTop = document.getElementById("chatDisplay").scrollHeight;
+        // document.getElementById("chatInput").value = "";
     }
 });
+
 
 socket.onmessage = (event) => {
     data = JSON.parse(event.data);
     // Tu dois parse la data que je t'envoie en fonction du type
     // if data['type'] == "update"
-    if (data['type'] == "infoS") log(data['dataS']);
+    if (data['type'] == "infoS") log(data['payload']);
     if (data['type'] == "init") {
-        const formattedHands = data['hands'].map(d => `(${d[0]}) (${d[1]}) (${d[2]}) (${d[3]}) (${d[4]}) (${d[5]}) (${d[6]})`).join(" ");
-        log("Vous êtes le joueur " + data['player']);
+        const formattedHands = data['hands'].map(d => `(${d[0]},${d[1]})`).join(" ");
         log("Main : " + formattedHands) ;
         log("Table vide : " + data['board']);
+        if (data['your_turn']){
+            message = "C'est à vous de jouer";
+        }else{
+            message = "C'est le tour de l'autre joueur";
+        }
+        log(message);
         document.getElementById("idPlayer").value = data['player'];
     } 
     if (data['type'] == "update") {
-        log("Table : " + data['board']);
-        log("Tour du joueur :" + data['current_player'])
+        log("Table : " + data['board'].map(d => `(${d[0]},${d[1]})`).join(" "));
+        if (data['your_turn']){
+            message = "C'est à vous de jouer";
+        }else{
+            message = "C'est le tour de l'autre joueur";
+        }
+        log(message);
+        log("Nouvelle Main : " + data['hands'].map(d => `(${d[0]},${d[1]})`).join(" "));
+      
     }
     if (data['type'] == "error") log(data['message']);
-    if (data['type'] == "win") {
-        log("Le joueur : " + data['player'] + " a gagné la partie!")
-    }
     if (data['type'] == "game_over") {
         log("Fin du jeu.");
+        log( data['winner'] + " a gagné la partie!") // Suly doit process l'info nomDuJoueur
+    }
+    if (data['type'] == "chat") {
+        //Define var 
+        const message = (data['message'])
+        const div = document.createElement('div');
+        const listChats = document.createElement('ul');
+        listChats.id = 'chatList';
+        const li = document.createElement('li')
+        ;
+        li.innerHTML = '<strong>' + data['sender'] + ' : </strong> ' + message;
+        document.getElementById("chatDisplay").append(li);
+        document.getElementById("chatDisplay").scrollTop = document.getElementById("chatDisplay").scrollHeight;
+    }
+    if (data['type'] == "clear") {
+        document.getElementById("log").textContent = "";
     }
 
     // if (data['init']) log(data['init'])
@@ -77,6 +133,8 @@ document.getElementById("disconnectBtn").addEventListener("click", () => {
     document.getElementById("connectBtn").style.display = "block";
     document.getElementById("joinBtn").style.display = "block";
     document.getElementById("disconnectBtn").style.display = "none";
+    document.getElementById("log").textContent = '';
+    playerName.style.display = 'block';
     });
 
 
@@ -96,7 +154,7 @@ document.getElementById("sideLBtn").addEventListener("click", () => {
     }
 
     const focus = document.getElementById("sideLBtn");
-    focus.style.color = "blue";
+    focus.style.color = "cyan";
 
     console.log(side)
 });
@@ -110,7 +168,7 @@ document.getElementById("sideRBtn").addEventListener("click", () => {
     }
 
     const focus = document.getElementById("sideRBtn");
-    focus.style.color = "blue";
+    focus.style.color = "cyan";
     console.log(side)
 
 });
@@ -118,6 +176,7 @@ document.getElementById("submitBtn").addEventListener("click", () => {
     const domino = document.getElementById("domino").value;
     const player = document.getElementById("idPlayer").value;
     socket.send(JSON.stringify({type : "play", "domino" : domino, "side" : side, "player" : player}));
+    document.getElementById("domino").value = "";
 });
 
 document.getElementById("passBtn").addEventListener("click", () => {
